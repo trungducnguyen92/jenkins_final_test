3f7c03fcbc344d43ad673dfb6eff7d681ae450d7
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-06 23:03:29 +0700

    Update index.js

--- nodejs-app/index.js
+++ nodejs-app/index.js
@@ -1,9 +1,26 @@
-const http = require('http');
+/*const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
+  res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});*/
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const app_env = process.env.APP_ENV || "unknown";
+const app_version = process.env.APP_VERSION || "unknown";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  //const msg = 'Hello Node!\n'
+  const msg = `Hello Word! Application version ${app_version} - Environment ${app_env}\n`
   res.end(msg);
 });
 
