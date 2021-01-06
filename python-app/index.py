6a1b979fa149306bc695ee689937b936e335f097
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 02:28:44 +0700

    Update index.py

--- python-app/index.py
+++ python-app/index.py
@@ -5,3 +5,11 @@ def hello():
     return "Hello World!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    
+/*from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)*/
