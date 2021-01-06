66ae26f9f746ec4a2a526d4eed8f6f99fc171247
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 00:55:55 +0700

    Update test.sh

--- nodejs-app/test.sh
+++ nodejs-app/test.sh
@@ -1,3 +1,7 @@
+echo '*************************'
+echo '****Running container****'
+echo '*************************'
+
 var1="$(docker ps -aq -f name=nodejsapp)"
 
 if [ -z "$var1" ]
@@ -5,6 +9,7 @@ then
 	HOST_NAME=$(hostname -f)
         docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name nodejsapp nodejs:latest
 else
+	echo 'Cleaning old container'
 	docker stop nodejsapp
         docker rm nodejsapp
 	HOST_NAME=$(hostname -f)
