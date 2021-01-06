d79911f458e58c50ad8b23182e115181048e0fb6
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 00:56:28 +0700

    Update Deploy.sh

--- nodejs-app/Deploy.sh
+++ nodejs-app/Deploy.sh
@@ -2,7 +2,16 @@ echo '*************************'
 echo '****Running container****'
 echo '*************************'
 
-docker stop nodejsapp
-docker rm nodejsapp
-HOST_NAME=$(hostname -f)
-docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name nodejsapp nodejs:latest
+var1="$(docker ps -aq -f name=nodejsapp)"
+
+if [ -z "$var1" ]
+then
+	HOST_NAME=$(hostname -f)
+        docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name nodejsapp nodejs:latest
+else
+	echo 'Cleaning old container'
+	docker stop nodejsapp
+        docker rm nodejsapp
+	HOST_NAME=$(hostname -f)
+        docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name nodejsapp nodejs:latest
+fi
