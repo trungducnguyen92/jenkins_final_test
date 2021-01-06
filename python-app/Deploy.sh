2d234b0b86dee2c0210f08e45a4f8afcb32d6c4d
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 03:36:38 +0700

    Update Deploy.sh

--- python-app/Deploy.sh
+++ python-app/Deploy.sh
@@ -3,6 +3,7 @@ echo '****Deploy container****'
 echo '*************************'
 
 IMAGE="pythonapp"
+HOST_NAME=$(hostname -f)
 var1="$(docker ps -aq -f name=pythonapp)"
 
 echo "** Logging in ***"
@@ -12,12 +13,10 @@ docker pull trungducnguyen/$IMAGE:latest
 
 if [ -z "$var1" ]
 then
-	HOST_NAME=$(hostname -f)
         docker container run -d -p 5000:5000 --name pythonapp trungducnguyen/$IMAGE:latest
 else
 	echo 'Cleaning old container'
 	docker stop pythonapp
         docker rm pythonapp
-	HOST_NAME=$(hostname -f)
         docker container run -d -p 5000:5000 --name pythonapp trungducnguyen/$IMAGE:latest
 fi
