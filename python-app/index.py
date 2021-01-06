938218d5aa19d0ef304b0eec26f9418187936694
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 03:35:35 +0700

    Update index.py

--- python-app/index.py
+++ python-app/index.py
@@ -1,25 +1,8 @@
-#!/usr/bin/env python3
-
-#from flask import Flask
-#app = Flask(__name__)
-#@app.route("/")
-#def hello():
-#    x = "Hello World! "
-#    y = "latest "
-#    z = "jenkins_2"
-#    return x,y,z
-#if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=int("5000"), debug=True)
-    
-#x,y,z = hello()
-#print(x)
-#print(y)
-#print(z)
 import os
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def hello(x = os.environ['APP_ENV']):
-    return "Hello World! " + x;
+def hello(x = os.environ['APP_ENV'], y = os.environ['APP_VERSION']):
+    return "Hello World! " + y + " " + x;
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
