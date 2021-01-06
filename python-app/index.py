43efa52bc1e4f15009250af19b88b97f5bdcd9cd
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 02:30:49 +0700

    Update index.py

--- python-app/index.py
+++ python-app/index.py
@@ -1,15 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
+x = latest
+y = jenkins_2
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print ("Hello World!")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
     
-/*from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)*/
+#from flask import Flask
+#app = Flask(__name__)
+#@app.route("/")
+#def hello():
+#    return "Hello World!"
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=int("5000"), debug=True)
