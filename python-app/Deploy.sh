ea5f0751610503d9697040417434be8648424837
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 02:11:04 +0700

    Update Deploy.sh

--- python-app/Deploy.sh
+++ python-app/Deploy.sh
@@ -2,8 +2,8 @@ echo '*************************'
 echo '****Deploy container****'
 echo '*************************'
 
-IMAGE="nodejsapp"
-var1="$(docker ps -aq -f name=nodejsapp)"
+IMAGE="pythonapp"
+var1="$(docker ps -aq -f name=pythonapp)"
 
 echo "** Logging in ***"
 docker login -u trungducnguyen -p $PASS
@@ -13,11 +13,11 @@ docker pull trungducnguyen/$IMAGE:latest
 if [ -z "$var1" ]
 then
 	HOST_NAME=$(hostname -f)
-        docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name nodejsapp trungducnguyen/$IMAGE:latest
+        docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name pythonapp trungducnguyen/$IMAGE:latest
 else
 	echo 'Cleaning old container'
-	docker stop nodejsapp
-        docker rm nodejsapp
+	docker stop pythonapp
+        docker rm pythonapp
 	HOST_NAME=$(hostname -f)
-        docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name nodejsapp trungducnguyen/$IMAGE:latest
+        docker container run -d -p 3000:3000 -e APP_VERSION="latest" -e APP_ENV=$HOST_NAME --name pythonapp trungducnguyen/$IMAGE:latest
 fi
