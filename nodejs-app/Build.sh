28ea509ecac7f88f89f53d9b756958d54653e452
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 01:51:21 +0700

    Update Build.sh

--- nodejs-app/Build.sh
+++ nodejs-app/Build.sh
@@ -2,4 +2,5 @@ echo '*********************************************'
 echo '**Build Docker image for nodejs application**'
 echo '*********************************************'
 
-cd nodejs-app/ && docker build -t nodejs:latest .
+IMAGE="nodejsapp"
+cd nodejs-app/ && docker build -t $IMAGE:latest .
