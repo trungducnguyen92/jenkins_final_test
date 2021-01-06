d0cfbda5abffd0c04722f99b6d43b14e53e02e68
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-07 02:55:29 +0700

    Update index.py

--- python-app/index.py
+++ python-app/index.py
@@ -20,6 +20,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    x = "Hello World!"
+    return x;
+x = hello()
+print(x)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
